acb9fc20bcfd6477a12b7c0966b0c338a765d3fa
gornovah <domingo.em@gmail.com>
2019-05-07 12:28:40 +0200

    roman numbers of maximu of three

--- src/main/java/RomanNumber.java
+++ src/main/java/RomanNumber.java
@@ -1,2 +1,11 @@
 public class RomanNumber {
+    public String convert(int number) {
+        String result = "";
+        if (number < 5) {
+            for (int i=0; i < number; i++){
+                result = result + "I";
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/RomanNumeralTest.java
+++ src/test/java/RomanNumeralTest.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class RomanNumeralTest {
+
+    @Test
+    public void convert_arabics_to_romans_first_number() {
+        RomanNumber romanNumberConverter = new RomanNumber();
+        String romanNumber = romanNumberConverter.convert(1);
+        assertThat(romanNumber, is("I"));
+    }
+
+    @Test
+    public void convert_arabics_to_romans_number_three() {
+        RomanNumber romanNumberConverter =new RomanNumber();
+        String romanNumber = romanNumberConverter.convert(3);
+        assertThat(romanNumber, is("III"));
+    }
+
+
 }
